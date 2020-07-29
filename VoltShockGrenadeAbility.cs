50f8d349d6d5f41844bb385b27e5c1f95337a724
KakolJakub <61240116+KakolJakub@users.noreply.github.com>
2020-07-29 20:25:26 +0200

    Updated TODO

--- VoltShockGrenadeAbility.cs
+++ VoltShockGrenadeAbility.cs
@@ -31,16 +31,18 @@ public class VoltShockGrenadeAbility : Ability
 	}
 	
 	//TODO:
-	//Implement animation - add an animator reference, add a SetTrigger
-	//Create electric particles - used when the grenade detonates
+	//Add gravity to the Shock Grenade Projectile (the gravity value should be set in this script)
+	//Add explosion radius to the Shock Grenade Projectile (the radius value should be set in this script)
 	//Make an enum for damageEffects (characterCombat script OR playerStats script)
 	
+	
+	//DONE: Create electric particles - used when the grenade detonates
+	//DONE: Implement animation - add an animator reference, add a SetTrigger
 	//DONE: Create a DealProjectileDamage method - used on collider collision (ProjectileLogic script)
 	//DONE: Create GameObject which will serve as a projectile spawn point and Attach it to the character
 	//DONE: Create a Projectile Logic Script
-	
-	//TESTING: Create a GameObject that will serve as a projectile (later used as a prefab)
-	//TESTING: Create a Projectile Spawn method, it will be used via animation event
+	//DONE: Create a GameObject that will serve as a projectile (later used as a prefab)
+	//DONE: Create a Projectile Spawn method, it will be used via animation event
 	
 	
 
