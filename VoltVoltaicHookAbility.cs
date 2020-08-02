b1344b9d1d671a72cf49d86406b00737fa238d54
KakolJakub <61240116+KakolJakub@users.noreply.github.com>
2020-08-02 13:44:18 +0200

    Added TODO

--- VoltVoltaicHookAbility.cs
+++ VoltVoltaicHookAbility.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public class VoltVoltaicHookAbility : Ability
 {
-   //TODO - 
+   //TODO:
+   //Create RopeLogic Script (or put that code here)
+   //Animator reference should probably be put in the base Ability script (since every ability needs an animator reference)
+   //Create animation trigger
 }
