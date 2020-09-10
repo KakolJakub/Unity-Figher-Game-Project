70802469acb3217138884134e85c1345038ed82a
KakolJakub <61240116+KakolJakub@users.noreply.github.com>
2020-09-10 21:58:41 +0200

    Set up methods for animation events

--- VoltVoltaicHookAbility.cs
+++ VoltVoltaicHookAbility.cs
@@ -3,24 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VoltVoltaicHookAbility : Ability
-{
-   //TODO: Complete animation
-   //TODO: Try Raycast approach
-   //OR:
-   //Create animation trigger
-   //Attach 2 gameObjects to the player (ropeBeginPoint and ropeEndPoint)
-   //Create RopeLogic Script (or put that code here):
-   //-> the ropeBeginPoint
-   //-> when spawned, the rope needs to extend in the direction of ropeEndPoint (change Transform values and SpriteRenderer values accordingly)
-   //-> when ropeEndPoint enters another Collider2D it stops extending and "attaches" to the Collider2D (NEEDS SOLVING)
-   //-> then the rope shortens in the direction of ropeEndPoint OR ropeBeginPoint (no decision yet)
-   //-> Player moves in the same direction
-   //-> At the end of animation destroy the rope object and stop moving the player
-   //TO SOLVE: Extending the rope only in one direction
-   //TO SOLVE: Attaching the ropeEndPoint to the hit Collider2D
-   
-   //DONE: Animator reference should be put in the base Ability script (since every ability needs an Animator reference)
-   
+{   
    public Transform ropeSpawnPoint;
    public Transform ropeTip;
    public LineRenderer rope;
@@ -30,30 +13,18 @@ public class VoltVoltaicHookAbility : Ability
    Transform actualRopeTip;
    bool attachRope;
    
-   //OLD
-   //string ropeTipName;
-   
    public override void ActivateAbility()
    {
+	   animate.ResetTrigger("Ability_VoltaicHook_Return");
+	   animate.ResetTrigger("Ability_VoltaicHook_Pull");
 	   animate.SetTrigger("Ability_VoltaicHook");
-	   //testing only:
-	   //VoltaicHook_Fire();
    }
    
    //used via animation event
    public void VoltaicHook_Fire()
    {
-	   //DONE: Instantiate RopeTip
-	   //DONE: LineRenderer follows RopeTip
-	   //DONE: When tip hits target it stops moving
-	   //SCRAPPED(done by VoltaicHook_Adjust): if it doesn't in X seconds it moves back
-	   //DONE: when tip hits target, perform VoltaicHook_Pull()
 	   
 	   SpawnRope();
-	   
-	   //IDEA: Make RopeTip a child of Player
-	   //Move the RopeTip via this script
-	   //By doing so, you avoid the chance of a bug appearing when another RopeTip appears
 	   
    }
    
@@ -66,20 +37,19 @@ public class VoltVoltaicHookAbility : Ability
    //used via animation event
    public void VoltaicHook_Adjust()
    {
+	   attachRope = GetRopeTipImpactInfo();
+	   
 	   if(attachRope)
 	   {
-		   Debug.Log("VH: Pull");
-		   //testing only:
-		   VoltaicHook_Pull();
-		   //animate.SetTrigger("VH_Pull").....
+		   animate.ResetTrigger("Ability_VoltaicHook_Return");
+		   animate.SetTrigger("Ability_VoltaicHook_Pull");
 	   }
 	   else
 	   {
-		   Debug.Log("VH: Return");
-		   //testing only:
-		   VoltaicHook_Retract();
-		   //animate.SetTrigger("VH_Return").....
+		   animate.ResetTrigger("Ability_VoltaicHook_Pull");
+		   animate.SetTrigger("Ability_VoltaicHook_Return");
 	   }
+	   
    }
    
    //used via animation event
@@ -94,26 +64,26 @@ public class VoltVoltaicHookAbility : Ability
    {
 	   DisarmRopeTip();
 	   MoveRopeTipBackwards();
-	   DestroyRopeTip();
+	   //DestroyRopeTip();
    }
    
    void FixedUpdate()
    {
-	   attachRope = GetRopeTipImpactInfo();
+	   
+	   //Debug.Log(attachRope);
 	   
 	   if(rope.enabled)
 	   {
 		   TrackRopeTip();
 	   }
-	   //Debug.Log(GetRopeTipPosition());
+	   
    }
    
    void Start()
    {
 	   attachRope = false;
 	   rope.enabled = false;
-	   //OLD
-	   //GetRopeTipName();
+	   
    }
    
    void SpawnRope()
@@ -154,14 +124,6 @@ public class VoltVoltaicHookAbility : Ability
 		   Debug.Log("RopeTip was erased.");
 	   }
    }
-   //OLD
-   /*
-   void GetRopeTipName()
-   {
-	   string clonePrefix = "(Clone)";
-	   ropeTipName = ropeTip.name + clonePrefix;
-   }
-   */
    
    bool GetRopeTipImpactInfo()
    {
@@ -178,21 +140,6 @@ public class VoltVoltaicHookAbility : Ability
 	   
 	   return ropeImpactInfo;
 	   
-	   //OLD
-	   /*
-	   bool ropeImpactInfo;
-	   
-	   if(GameObject.Find(ropeTipName) == null)
-	   {
-		   ropeImpactInfo = false;
-	   }
-	   else
-	   {
-		   ropeImpactInfo = GameObject.Find(ropeTipName).GetComponent<ProjectileLogic>().Impact;
-	   }
-	   
-	   return ropeImpactInfo;
-	   */
    }
    
    Vector3 GetRopeTipPosition()
@@ -210,20 +157,5 @@ public class VoltVoltaicHookAbility : Ability
 	   
 	   return ropeTipPosition;
 	   
-	   //OLD
-	   /*
-	   Vector3 ropeTipPosition;
-	   
-	   if(GameObject.Find(ropeTipName) == null)
-	   {
-		   ropeTipPosition = ropeSpawnPoint.position;
-	   }
-	   else
-	   {
-		   ropeTipPosition = GameObject.Find(ropeTipName).transform.position;
-	   }
-	   
-	   return ropeTipPosition;
-	   */
    }
 }
