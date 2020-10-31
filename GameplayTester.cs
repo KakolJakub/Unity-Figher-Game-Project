7f8499037530bb9a068799e843653f2a88bd286e
KakolJakub <61240116+KakolJakub@users.noreply.github.com>
2020-10-31 23:34:44 +0100

    Added a note

--- GameplayTester.cs
+++ GameplayTester.cs
@@ -72,6 +72,7 @@ public class GameplayTester : MonoBehaviour
 	{
 		PlayerStats.OnDeath -= RoundEnd;
 		videoPlayer.loopPointReached -= EndRageCutscene;
+		//TODO: Add a nullguard for this:
 		players[0].GetComponent<RageMode>().OnRageCutsceneStart -= PlayRageCutscene;
 		//players[1].GetComponent<RageMode>().OnRageCutsceneStart -= PlayRageCutscene;
 	}
